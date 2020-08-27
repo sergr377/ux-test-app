544132efab5142217c69d522f43cd7a0eb23b863
Sergey <sergr377@gmail.com>
2020-08-27 18:35:50 +0300

    Updates

---
+++ precache-manifest.b5ea4d209676de9566f3a3615b6d320a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66b73a89647b6ec37bee597708c7a35",
+    "url": "/ux-test-app/index.html"
+  },
+  {
+    "revision": "ba7d8e82057eb143df87",
+    "url": "/ux-test-app/static/css/main.c4ed61c1.chunk.css"
+  },
+  {
+    "revision": "2dc2805ed6268c75ae59",
+    "url": "/ux-test-app/static/js/2.1786865b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ux-test-app/static/js/2.1786865b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba7d8e82057eb143df87",
+    "url": "/ux-test-app/static/js/main.a6fa1a54.chunk.js"
+  },
+  {
+    "revision": "403ac7867b67ea59389a",
+    "url": "/ux-test-app/static/js/runtime-main.f3e6f69b.js"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/ux-test-app/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/ux-test-app/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "f8fbd17487884ec159862ac6dbd95470",
+    "url": "/ux-test-app/static/media/Illustration - Архитектура.f8fbd174.png"
+  },
+  {
+    "revision": "5ec80abc4b96cce87edd7bb52a72537b",
+    "url": "/ux-test-app/static/media/Illustration - Благоустройство.5ec80abc.png"
+  },
+  {
+    "revision": "d1e41d87ab98b920334dc42194e31116",
+    "url": "/ux-test-app/static/media/Illustration - Инженерия.d1e41d87.png"
+  },
+  {
+    "revision": "e9740cd0e8230ecb4a2bbb80246578fc",
+    "url": "/ux-test-app/static/media/Illustration - Инфраструктура.e9740cd0.png"
+  },
+  {
+    "revision": "8a06801250e454e3fba1b9fe01eac8fd",
+    "url": "/ux-test-app/static/media/Illustration - Транспортная доступность.8a068012.png"
+  },
+  {
+    "revision": "666723028d3d17bb486df7f4b78dacb5",
+    "url": "/ux-test-app/static/media/IllustrationMask - Безопастность.66672302.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9e6a2e47edb64cee24aea11734f98845.js"
+  "/ux-test-app/precache-manifest.b5ea4d209676de9566f3a3615b6d320a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ux-test-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
